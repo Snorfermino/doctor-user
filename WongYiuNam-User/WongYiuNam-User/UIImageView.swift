f762e35168af80d6da89efa1a74df9e47fc767e5
Duy <admin@Admins-MacBook-Pro.local>
2017-09-08 10:19:22 +0700

    left menu + nav

--- WongYiuNam-User/WongYiuNam-User/UIImageView.swift
+++ WongYiuNam-User/WongYiuNam-User/UIImageView.swift
@@ -11,41 +11,6 @@ import UIKit
 
 extension UIImageView {
     
-    func setRandomDownloadImage(_ width: Int, height: Int) {
-        if self.image != nil {
-            self.alpha = 1
-            return
-        }
-        self.alpha = 0
-        let url = URL(string: "http://lorempixel.com/\(width)/\(height)/")!
-        let configuration = URLSessionConfiguration.default
-        configuration.timeoutIntervalForRequest = 15
-        configuration.timeoutIntervalForResource = 15
-        configuration.requestCachePolicy = NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData
-        let session = URLSession(configuration: configuration)
-        let task = session.dataTask(with: url) { (data, response, error) in
-            if error != nil {
-                return
-            }
-            
-            if let response = response as? HTTPURLResponse {
-                if response.statusCode / 100 != 2 {
-                    return
-                }
-                if let data = data, let image = UIImage(data: data) {
-                    DispatchQueue.main.async(execute: { () -> Void in
-                        self.image = image
-                        UIView.animate(withDuration: 0.3, animations: { () -> Void in
-                            self.alpha = 1
-                            }, completion: { (finished: Bool) -> Void in
-                        }) 
-                    })
-                }
-            }
-        }
-        task.resume()
-    }
-    
     func clipParallaxEffect(_ baseImage: UIImage?, screenSize: CGSize, displayHeight: CGFloat) {
         if let baseImage = baseImage {
             if displayHeight < 0 {
@@ -65,5 +30,10 @@ extension UIImageView {
             self.image = baseImage.trim(trimRect: trimRect)
             self.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: displayHeight)
         }
+    }
+    
+    func scale(f: CGFloat) {
+        self.frame.size.width *= f
+        self.frame.size.height *= f
     }
 }
--- WongYiuNam-User/WongYiuNam-User/UIViewController.swift
+++ WongYiuNam-User/WongYiuNam-User/UIViewController.swift
@@ -12,6 +12,13 @@ extension UIViewController {
     
     func setNavigationBarItem() {
         self.addLeftBarButtonWithImage(UIImage(named: "ic_menu_black_24dp")!)
+        
+        let logoImageView = UIImageView(image: UIImage(named: "logo"))
+        logoImageView.scale(f: 0.5)
+        let view = UIView(frame: CGRect(x: -80, y: 10, width: 120, height: 80))
+        view.addSubview(logoImageView)
+        self.navigationItem.titleView = view
+        
         self.slideMenuController()?.removeLeftGestures()
         self.slideMenuController()?.addLeftGestures()
     }

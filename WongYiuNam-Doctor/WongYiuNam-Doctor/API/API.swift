fb14200b876d62a9370fb911708cba1b25efade6
Snor <dat.snor2911@gmail.com>
2017-09-15 14:29:40 +0700

    moya formdata

--- WongYiuNam-Doctor/WongYiuNam-Doctor/API/API.swift
+++ WongYiuNam-Doctor/WongYiuNam-Doctor/API/API.swift
@@ -9,7 +9,33 @@
 import Moya
 import Foundation
 import Result
-
+class DebugPlugin: PluginType {
+    
+    static let targetKey = "target"
+    /// Called by the provider as soon as the request is about to start
+    func willSend(_ request: RequestType, target: TargetType) {
+        if let request = request as? CustomDebugStringConvertible {
+            
+            print(request.debugDescription)
+        }
+    }
+    
+    func didReceive(_ result: Result<Moya.Response, Moya.MoyaError>, target: TargetType) {
+        switch result {
+        case let .success(response):
+            print(response.statusCode)
+            do {
+                let json = try response.mapJSON()
+                print(json)
+            } catch {
+                print(error.localizedDescription)
+            }
+        case let .failure(error):
+            print(error.localizedDescription)
+            break
+        }
+    }
+}
 public enum API: TargetType{
     
     case login(email: String, passwd: String)
@@ -42,7 +68,22 @@ extension API {
     }
     var parameters: [String: Any]? {
         switch self {
-            
+        case .login(let email, let pwd):
+//
+//             do {
+//                let jsonData = try JSONSerialization.data(withJSONObject: ["email":email,"password":pwd], options: .prettyPrinted)
+//                // here "jsonData" is the dictionary encoded in JSON data
+//                
+//
+//                // here "decoded" is of type `Any`, decoded from JSON data
+//                
+//                // you can now cast it with the right type
+//                let data = MultipartFormData(provider: 	.data(jsonData), name: "something"), name: "something")
+//                return data
+//             } catch {
+//                print(error.localizedDescription)
+//             }
+            return ["email":email,"password":pwd]
         default:
             return nil
         }
@@ -53,13 +94,28 @@ extension API {
     public var task: Task {
         switch self {
         default:
-            return .requestPlain
+            return .requestParameters(parameters: self.parameters!, encoding: JSONEncoding(options: []))
         }
     }
     public var sampleData: Data {
-        return Data()
+        switch self {
+        default:
+            return "".data(using: String.Encoding.utf8)!
+        }
     }
     
     
 }
-let apiProvider
+let endPoint = { (target: API) -> Endpoint<API> in
+    let url = target.baseURL.appendingPathComponent(target.path).absoluteString
+    var header: [String:String] = [:]
+    header["X-App-Token"] = "Ly93b25neWl1bmFtLXBocC5oZXJva3VhcHAuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF"
+    header["Content-Type"] = "application/x-www-form-urlencoded"
+    return Endpoint<API>(url: url,
+                         sampleResponseClosure: {.networkResponse(200, target.sampleData)},
+                         method: target.method,
+                         task: target.task,
+                         httpHeaderFields: header)
+}
+let apiProvider = MoyaProvider<API>(endpointClosure: endPoint,
+                                    plugins: [DebugPlugin()])
---
+++ WongYiuNam-Doctor/WongYiuNam-Doctor/View/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  WongYiuNam-Doctor
+//
+//  Created by Admin on 9/15/17.
+//  Copyright © 2017 RTH. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var tfEmail:UITextField!
+    @IBOutlet weak var tfPassword:UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSignInPressed(_ sender: UIButton){
+        print("email: \(tfEmail.text!), password: \(tfPassword.text!)" )
+        apiProvider.request(.login(email: tfEmail.text!,passwd: tfPassword.text!)) { (result) in
+            switch result {
+            case let .success(response):
+                print(response)
+            case .failure:
+                print("failed")
+            }
+
+        }
+    
+    }
+
+}

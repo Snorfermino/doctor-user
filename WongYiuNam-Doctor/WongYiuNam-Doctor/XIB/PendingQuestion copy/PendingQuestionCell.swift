43450bbb7a62ae4fbf285ac7c69d83ff2031179a
Snor <dat.snor2911@gmail.com>
2017-09-26 12:54:15 +0700

    switch

---
+++ WongYiuNam-Doctor/WongYiuNam-Doctor/XIB/PendingQuestion copy/PendingQuestionCell.swift
@@ -0,0 +1,20 @@
+//
+//  PendingQuestion.swift
+//  WongYiuNam-Doctor
+//
+//  Created by Admin on 9/13/17.
+//  Copyright © 2017 RTH. All rights reserved.
+//
+
+import UIKit
+
+class PendingQuestionCell: UITableViewCell {
+
+    @IBOutlet weak var tvQuestion:UITextView!
+    @IBOutlet weak var lbCreatedAt:UILabel!
+    @IBOutlet weak var lbPatientName:UILabel!
+    @IBOutlet weak var lbPatientGender:UILabel!
+    @IBOutlet weak var lbPatientDOB:UILabel!
+    @IBOutlet weak var imgViewPatientSubmit:UIImageView!
+
+}

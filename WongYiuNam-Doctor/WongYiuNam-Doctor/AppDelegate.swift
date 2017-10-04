8afd312163bf0925724ad232b7acca66045887b0
Snor <dat.snor2911@gmail.com>
2017-10-04 16:56:23 +0700

    remove fcm print

--- WongYiuNam-Doctor/WongYiuNam-Doctor/AppDelegate.swift
+++ WongYiuNam-Doctor/WongYiuNam-Doctor/AppDelegate.swift
@@ -18,8 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let token = Messaging.messaging().fcmToken
-        print("FCM token: \(token ?? "")")
+
         registerRemoteNotification(application)
         registerForPushNotifications(application)
         

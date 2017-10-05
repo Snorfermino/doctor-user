855ff914ca9a3919dfa48b3c122ec04cd32454ec
Duy <admin@Admins-MacBook-Pro-2.local>
2017-10-05 14:21:51 +0700

    fixed all bugs

---
+++ WongYiuNam-User/WongYiuNam-User/Models/Social/PagingResponseFB.swift
@@ -0,0 +1,9 @@
+//
+//  PagingResponseFB.swift
+//  WongYiuNam-User
+//
+//  Created by Admin on 10/5/17.
+//  Copyright © 2017 RTH. All rights reserved.
+//
+
+import Foundation
---
+++ WongYiuNam-User/WongYiuNam-User/Models/Social/PostFB.swift
@@ -0,0 +1,9 @@
+//
+//  PostFB.swift
+//  WongYiuNam-User
+//
+//  Created by Admin on 10/5/17.
+//  Copyright © 2017 RTH. All rights reserved.
+//
+
+import Foundation

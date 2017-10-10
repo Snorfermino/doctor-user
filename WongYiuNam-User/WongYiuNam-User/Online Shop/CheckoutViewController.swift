4b2c28cc7d2a876bf1ec6b24c9c4490895062882
Phat Chiem <thachphat0@gmail.com>
2017-10-10 11:25:28 +0700

    clean code

--- WongYiuNam-User/WongYiuNam-User/Online Shop/CheckoutViewController.swift
+++ WongYiuNam-User/WongYiuNam-User/Online Shop/CheckoutViewController.swift
@@ -10,18 +10,6 @@ import UIKit
 import BraintreeDropIn
 import Braintree
 class CheckoutViewController: BaseViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
     func getPaymentToken(){
         let completion = { [unowned self] (paymentToken: String?,error: String?) in

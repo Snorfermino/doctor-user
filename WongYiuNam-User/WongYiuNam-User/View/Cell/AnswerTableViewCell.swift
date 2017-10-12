ff18824c18d24e286f0d4eaa4670a68e1bf4fdcb
Duy <admin@Admins-MacBook-Pro-2.local>
2017-10-12 18:20:49 +0700

    add DateToolsSwift

--- WongYiuNam-User/WongYiuNam-User/View/Cell/AnswerTableViewCell.swift
+++ WongYiuNam-User/WongYiuNam-User/View/Cell/AnswerTableViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import DateToolsSwift
 
 class AnswerTableViewCell: UITableViewCell {
 
@@ -23,7 +24,7 @@ class AnswerTableViewCell: UITableViewCell {
     func setup(question: Question) {
         nameLabel.text = question.patientName
         questionLabel.text = question.question
-        timeLabel.text = question.createdAt?.toString(format: "MM-dd-yyyy")
+        timeLabel.text = question.createdAt!.format(with: "dd-MM-yyyy")
         avatarImageView.sd_setImage(with: question.photoUrl, completed: nil)
     }
 }
